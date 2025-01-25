21ba9542bd7c877bd89633e0d40cfae28a9c1d09
sajid189210 <sajid189210@gmail.com>
2025-01-25 12:17:14 +0530

    added google auth

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,15 +1,15 @@
 import { Routes } from '@angular/router';
 
 export const routes: Routes = [
-    // {
-    //     path: '',
-    //     loadComponent: async () => {
-    //         const c = await import('../app/login/login.component');
-    //         return c.LoginComponent;
-    //     }
-    // },
     {
         path: '',
+        loadComponent: async () => {
+            const c = await import('../app/pages/login/login.component');
+            return c.LoginComponent;
+        }
+    },
+    {
+        path: 'browse',
         loadComponent: async () => {
             const c = await import('./pages/browse/browse.component');
             return c.BrowseComponent;
